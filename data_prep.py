5df6c19a0d889d60f5da491980dadaf92f2ec5fb
Markus Kasten <github@markuskasten.eu>
2021-02-07 20:57:14 +0100

    Create utils to read labelme data and display datasets

--- data_prep.py
+++ data_prep.py
@@ -1,4 +1,7 @@
+import json
 import tensorflow as tf
+import numpy as np
+import cv2
 
 from model import INPUT_SHAPE
 
@@ -27,3 +30,36 @@ def cityscapes_prep(output_shape, class_map_func=None, float_range=True):
         return img, seg
     
     return prep_map
+
+
+def create_labelme_segmentation(contents):
+    meta = json.loads(contents.numpy().decode('utf-8'))
+    seg = np.zeros((meta['imageHeight'], meta['imageWidth']))
+    
+    # TODO: create a good way to have multiple classes
+    for shape in meta['shapes']:
+        points = np.array(shape['points']).astype(np.int32)
+        cv2.fillPoly(seg, [points], (1))
+    
+    return seg, meta['imageHeight'], meta['imageWidth']
+
+
+def labelme_prep(output_shape, float_range=True):
+    def labelme_map(json_file):
+        contents = tf.io.read_file(json_file)
+        seg, h, w = tf.py_function(create_labelme_segmentation, [contents], [tf.float32, tf.int32, tf.int32])
+        seg = tf.reshape(seg, (h, w, 1))
+        seg = tf.image.resize(seg, output_shape[0:2])
+        seg = tf.where(seg == 1, [0, 1.0], [1.0, 0])
+        
+        jpeg_filename = tf.strings.regex_replace(json_file, '\.json', '.jpg')
+        jpeg_contents = tf.io.read_file(jpeg_filename)
+        img = tf.io.decode_jpeg(jpeg_contents, channels=3)
+        img = tf.image.resize(img, INPUT_SHAPE[0:2])
+        
+        if float_range:
+            img /= 255
+        
+        return img, seg
+    
+    return labelme_map
--- utils.py
+++ utils.py
@@ -1,7 +1,28 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 
 def segmentation_to_image(pred):
     img = tf.argmax(pred, axis=-1)
     img = img[..., tf.newaxis]
     return tf.keras.preprocessing.image.array_to_img(img)
+
+        
+def predict_tf(model):
+    def predict_func(sample):
+        pred = model.predict(tf.expand_dims(sample[0], axis=0))
+        return sample[0], pred[0]
+    
+    return predict_func
+
+
+def display_dataset(ds, pred_func):
+    for sample in ds:
+        imgs = pred_func(sample)
+        fig, axes = plt.subplots(1, len(imgs))
+        
+        for ax, img in zip(axes, imgs):
+            if img.shape[-1] != 3:
+                img = segmentation_to_image(img)
+                
+            ax.imshow(img)
