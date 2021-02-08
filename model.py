b5df816af8a25709388ed64df2acd6e8c16b3831
Markus Kasten <github@markuskasten.eu>
2021-02-08 18:35:52 +0100

    Set a better default seghead expand ratio

--- model.py
+++ model.py
@@ -143,7 +143,7 @@ class ArgmaxMeanIOU(metrics.MeanIoU):
         return super().update_state(tf.argmax(y_true, axis=-1), tf.argmax(y_pred, axis=-1), sample_weight)
 
 
-def bisenetv2(num_classes=2, out_scale=2, input_shape=INPUT_SHAPE, l=4, c_t=128):
+def bisenetv2(num_classes=2, out_scale=8, input_shape=INPUT_SHAPE, l=4, seghead_expand_ratio=2):
     x_in = layers.Input(input_shape)
 
     # semantic branch
@@ -184,7 +184,7 @@ def bisenetv2(num_classes=2, out_scale=2, input_shape=INPUT_SHAPE, l=4, c_t=128)
 
     x = bilateral_guided_aggregation(y, x, 128)
 
-    x = seg_head(x, c_t, out_scale, num_classes)
+    x = seg_head(x, num_classes * seghead_expand_ratio, out_scale, num_classes)
     
     model = models.Model(inputs=[x_in], outputs=[x])
     
