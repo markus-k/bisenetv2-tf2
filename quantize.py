e545576697844d89797cb20748a978e99472d5fa
Markus Kasten <github@markuskasten.eu>
2021-02-09 12:12:10 +0100

    Improve quantize script documentation

--- quantize.py
+++ quantize.py
@@ -9,10 +9,14 @@ from model import ArgmaxMeanIOU
 from data_prep import cityscapes_prep, class_map_road
 
 
-parser = argparse.ArgumentParser(description='Quantize BiSeNetV2 .tf model')
-parser.add_argument('tf_model')
-parser.add_argument('quant_model')
-parser.add_argument('--cpu', action='store_true')
+parser = argparse.ArgumentParser(description='Quantize BiSeNetV2 .tf model for TF-Lite.')
+parser.add_argument('tf_model', help='Unquantized TensorFlow .tf model directory.')
+parser.add_argument('quant_model', help='Quantized TF-Lite .tflite output file.')
+parser.add_argument('--cpu', action='store_true', help='Disable claiming CUDA devices. ' + \
+                    'Useful when running Notebooks or other scripts in the background.')
+parser.add_argument('--mlir', action='store_true', help='Enable the new MLIR converter. ' + \
+                    'Not working at this point, but may be an option once it\'s supported by ' + \
+                    'the EdgeTPU compiler.')
 
 args = parser.parse_args()
 
@@ -41,7 +45,7 @@ converter.representative_dataset = representative_dataset
 converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
 converter.inference_input_type = tf.uint8
 converter.inference_output_type = tf.uint8
-converter.experimental_new_converter = False # https://github.com/google-coral/edgetpu/issues/168#issuecomment-656115637
+converter.experimental_new_converter = args.mlir # https://github.com/google-coral/edgetpu/issues/168#issuecomment-656115637
 tflite_quant_model = converter.convert()
 
 with open(args.quant_model, 'wb') as fd:
