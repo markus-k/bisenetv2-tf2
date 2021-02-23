2ba5b980c46f5a90316b56e3e81fd16cff35f5fc
Markus Kasten <github@markuskasten.eu>
2021-02-23 13:08:30 +0100

    Also benchmark set_tensor/get_tensor
    Makes no difference really

--- edgetpu_benchmark.py
+++ edgetpu_benchmark.py
@@ -46,26 +46,41 @@ print(input_details)
 print(output_details)
 
 times = []
+times2 = []
 first = True
 
 for i in range(COUNT+1):
     shape = (1, *INPUT_SHAPE)
     input_data = np.zeros(shape, dtype=np.uint8)
 
+    s1 = time.time()
     interpreter.set_tensor(input_details[0]['index'], input_data)
 
-    print('Invoking.. ', end='')
-
-    s = time.time()
+    s2 = time.time()
     interpreter.invoke()
-    e = time.time()
+    e2 = time.time()
+
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    e1 = time.time()
+
     if not first:
-        times.append(e-s)
+        times.append(e2-s2)
+        times2.append(e1-s1)
     else:
         first = False
-    print(f'invoke: {e-s:.3f}s ({1/(e-s):.2f} fps)')
-    output_data = interpreter.get_tensor(output_details[0]['index'])
 
+    print(f'invoke: {e2-s2:.3f}s ({1/(e2-s2):.2f} fps)')
+    print(f'invoke+load: {e1-s1:.3f}s ({1/(e1-s1):.2f} fps)')
+
+
+print()
+print('Invoke:')
 invoke_avg = sum(times) / len(times)
 print(f'Average: {invoke_avg:.3f}s ({1/invoke_avg:.2f} fps)')
 print(f'min/max/stdev: {min(times):.03f}/{max(times):.03f}/{statistics.stdev(times):.03f}')
+
+print()
+print('Total:')
+total_avg = sum(times2) / len(times2)
+print(f'Average: {total_avg:.3f}s ({1/total_avg:.2f} fps)')
+print(f'min/max/stdev: {min(times2):.03f}/{max(times2):.03f}/{statistics.stdev(times2):.03f}')
